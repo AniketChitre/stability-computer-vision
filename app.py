a97dbc56708254b5d9ea2f282295434ff32cb778
Aniket <ac2349@cam.ac.uk>
2023-05-03 19:38:33 +0800

    Cleaning up

--- app.py
+++ app.py
@@ -16,18 +16,19 @@ def upload_file():
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
         image = Image.open(uploaded_file)
-        image.save('./static/this.png')
+        image.save('/home/Aket95/mysite/static/this.png')
         #call infer.py here
-        result, conf_score = infer_one(imgpath='./static/this.png')
+        result, conf_score = infer_one(imgpath='/home/Aket95/mysite/static/this.png')
         ##############################
         contents = '_'.join(uploaded_file.filename.split('_')[2:3]) + ' is '
-        contents += 'True' if result else 'False'
+        contents += 'stable' if result else 'unstable'
         contents += ' with ' + str(conf_score) + f'% confidence'
         return render_template('index.html', contents=contents)
     else:
         return render_template('index.html', contents=None)
-    
+
 
 if __name__ == '__main__':
+    print('WORK DIR', os.getcwd())
     app.run(debug=False)
 
--- infer.py
+++ infer.py
@@ -1,4 +1,3 @@
-
 from objdet.yolov5.detect import run
 from classification.trainer import classify
 import os
@@ -6,7 +5,7 @@ import glob
 import shutil
 
 def infer_one(imgpath):
-    weights = './objdet/aoi_model/best.pt'    # trained crop detection model weights
+    weights = '/home/Aket95/mysite/objdet/aoi_model/best.pt'    # trained crop detection model weights
     imgsz = (640, 640)  # raw data image size
     conf_thres = 0.90   # required confidence of formulation object detection
     max_det = 1         # maximum 1 formulation sample per image
@@ -37,7 +36,7 @@ if __name__ == '__main__':  # allows you to execute code when file runs as a scr
     for imgpath in glob.glob('/Users/ac2349/GitHub/stability-computer-vision/data/test_set/*.png'):
         source = imgpath
         mycroppath = run(weights=weights, imgsz=imgsz, conf_thres=conf_thres, max_det=max_det, save_crop=save_crop, source=source)
-        print('cropped image path: ', mycroppath)    
+        print('cropped image path: ', mycroppath)
         result = classify(mycroppath)
         print('classification result: ', 'True' if result else 'False')
 
